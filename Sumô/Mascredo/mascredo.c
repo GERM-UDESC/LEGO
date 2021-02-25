0684bdb30b8df8a88035ebc9b94c5f995fe386f1
RodrigoWBaggio <49909515+RodrigoWBaggio@users.noreply.github.com>
2021-02-25 20:30:16 -0300

    Update mascredo.c

--- Sumô/Mascredo/mascredo.c
+++ Sumô/Mascredo/mascredo.c
@@ -63,14 +63,14 @@ task main()
 				setMotorSpeed(dir, 55);
 				vira = 0;
 			}
-			if((getUSDistance(U1) > 40) && (getUSDistance(U2) > 40) && (vira == 1)){
+			if((getUSDistance(U1) > 40) && (getUSDistance(U2) > 40)/* && (vira == 1)*/){
 
 				setMotorSpeed(esq, -50);
 				setMotorSpeed(dir, 50);
 			}
-			if((getUSDistance(U1) > 40) && (getUSDistance(U2) > 40) && (vira == 0)){
+			/*if((getUSDistance(U1) > 40) && (getUSDistance(U2) > 40) && (vira == 0)){
 				setMotorSpeed(esq, 50);
-				setMotorSpeed(dir, -50);
+				setMotorSpeed(dir, -50);*/
 			}
 		}
 		if((getColorReflected(C1) < 10) && (getColorReflected(C2) > 10)){
